51d3ed12d891e8a188a3209ca78c9b05a7a17c96
Michael <me@miikey.pw>
2022-11-13 15:22:45 +1100

    refactor: moved path.join to a reusable function

--- examples/server.ts
+++ examples/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 
 import { registerRoutes } from "../lib/register";
 
@@ -8,17 +9,20 @@ const port = 5050;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const routes = registerRoutes(app, {
-    directory: "examples/routes",
-    appMount: "/api"
-    // indexNames: ["index"],
-    // developmentRoutes: ["user"],
-    // regexURLs: true
-    // silent: true
+function join(name: string): string {
+    return path.join(__dirname, name);
+}
+
+registerRoutes(app, {
+    directory: join("routes"),
+    appMount: "/api",
+    output: join(".fs-routes"),
+    environmentRoutes: {
+        development: [join("routes")]
+    },
+    silent: true
 });
 
-// console.log(routes);
-
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
