ea1660e3a0d22ca9bb05446303ff5d8a9311e9e2
Michael <me@miikey.pw>
2022-11-14 12:05:34 +1100

    test: unit tests i'll come back to later

---
+++ test/fs-routes.test.ts
@@ -0,0 +1,9 @@
+import chai from "chai";
+import request from "supertest";
+import envAgent from "env-agent";
+
+import { registerRoutes } from "../lib/register";
+
+const expect = chai.expect;
+
+describe("fs-routes", () => {});
