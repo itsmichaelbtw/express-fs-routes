c8c7bad0b0dec05b33d4ec8354238a9c0df8aa6d
Michael <me@miikey.pw>
2022-11-14 15:05:58 +1100

    chore: express server testing scripts

---
+++ scripts/server.ts
@@ -0,0 +1,20 @@
+import http from "http";
+import express from "express";
+import envAgent from "env-agent";
+
+export function spawnServer(): express.Application {
+    const app = express();
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    return app;
+}
+
+export function startServer(app: express.Application): http.Server {
+    return app.listen(envAgent.get("PORT"));
+}
+
+export function closeServer(server: http.Server): void {
+    server.close();
+}
