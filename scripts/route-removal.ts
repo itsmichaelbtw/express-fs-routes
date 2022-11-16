07a690b4c48aa616b6f91732be40fe162de5bfc4
Michael <me@miikey.pw>
2022-11-16 13:13:14 +1100

    chore: fixed incorrect express type for app

--- scripts/route-removal.ts
+++ scripts/route-removal.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-export function destroyAllRoutes(app: express.Express): void {
+export function destroyAllRoutes(app: express.Application): void {
     app._router.stack = app._router.stack.filter((layer: any) => {
         return layer.name !== "router";
     });
