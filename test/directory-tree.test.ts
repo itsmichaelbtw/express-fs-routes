906da333e1f23ddda2946e90a40ccd1573a2dc2c
Michael <me@miikey.pw>
2023-02-02 12:29:19 +1100

    test: rewrote unit tests to support new routing engine

--- test/directory-tree.test.ts
+++ test/directory-tree.test.ts
@@ -7,15 +7,15 @@ function join(...pathName: string[]): string {
     return path.join(__dirname, "..", ...pathName);
 }
 
-describe("directoryTree()", () => {
-    it("should create a directory tree", () => {
-        const examplesDir = join("examples/routes");
+describe("createDirectoryTree", () => {
+    it("should create a directory tree", async () => {
+        const examplesDir = join("examples/commonjs/routes");
 
-        const directoryTree = createDirectoryTree(examplesDir, () => {});
+        const directoryTree = await createDirectoryTree(examplesDir, async () => {});
 
         chai.expect(directoryTree).to.be.an("object");
         chai.expect(directoryTree).to.have.property("name");
-        chai.expect(directoryTree).to.have.property("path");
+        chai.expect(directoryTree).to.have.property("absolute_path");
         chai.expect(directoryTree).to.have.property("type");
         chai.expect(directoryTree).to.have.property("children");
     });
