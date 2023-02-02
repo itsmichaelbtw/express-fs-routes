d3b22031eceadfccd998843b298bf83dd8b1a3d8
Michael <me@miikey.pw>
2023-02-02 12:50:13 +1100

    feat: revised file output with file path redact support

--- lib/output.ts
+++ lib/output.ts
@@ -1,27 +1,90 @@
 import fs from "fs";
 import path from "path";
 
+import type { RouteRegistry, DirectoryTree } from "./types";
+
 import { debug } from "./debug";
+import { isArray } from "./utils";
 
-export function checkOutputDir(outputDir: string, callback: Function) {
-    if (!fs.existsSync(outputDir)) {
-        fs.mkdirSync(outputDir);
+interface OutputOptions {
+    data: RouteRegistry | DirectoryTree;
+    fileName: string;
+}
+type OutputCallback = () => [OutputOptions, OutputOptions];
+
+export class Output {
+    /**
+     * Ensures the output directory exists and writes the files to it.
+     *
+     * @param directory The output directory.
+     * @param callback The callback to generate the files.
+     */
+    static ensureOutputDir(directory: string, callback: OutputCallback): void {
+        if (!fs.existsSync(directory)) {
+            fs.mkdirSync(directory);
+        }
+
+        const files = callback();
+
+        for (const file of files) {
+            try {
+                const filePath = path.resolve(path.join(directory, file.fileName));
+
+                fs.writeFileSync(filePath, JSON.stringify(file.data, null, 2), {
+                    encoding: "utf8",
+                    flag: "w"
+                });
+            } catch (error) {
+                debug(error, "red");
+            }
+        }
     }
-
-    callback(outputDir);
 }
 
-export function saveOutputToDisk(location: string, data: any, fileName: string): void {
-    try {
-        const filePath = path.resolve(path.join(location, fileName));
+export class Redact {
+    /**
+     * Redacts the absolute path from the route registry nodes.
+     *
+     * @param registry The route registry.
+     * @param redact Whether to redact the absolute path.
+     * @returns The redacted route registry.
+     */
+    static routeRegistry(registry: RouteRegistry, redact: boolean): RouteRegistry {
+        if (redact) {
+            return registry.map((entry) => {
+                return {
+                    ...entry,
+                    absolute_path: "..."
+                };
+            });
+        }
 
-        data = JSON.stringify(data, null, 2);
+        return registry;
+    }
 
-        fs.writeFileSync(filePath, data, {
-            encoding: "utf8",
-            flag: "w"
-        });
-    } catch (error) {
-        debug(error, "red");
+    /**
+     * Redacts the absolute path from the directory tree nodes.
+     *
+     * @param tree The directory tree.
+     * @param redact Whether to redact the absolute path.
+     * @returns The redacted directory tree.
+     */
+    static routeTree(tree: DirectoryTree, redact: boolean): DirectoryTree {
+        if (redact) {
+            const updatedNode = {
+                ...tree,
+                absolute_path: "..."
+            };
+
+            if (isArray(tree.children)) {
+                updatedNode.children = tree.children.map((child) => {
+                    return Redact.routeTree(child, redact);
+                });
+            }
+
+            return updatedNode;
+        }
+
+        return tree;
     }
 }
