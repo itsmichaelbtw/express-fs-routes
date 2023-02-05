b7366d37c90a9c2f2bc6c7564f805824b720c75b
Michael <me@miikey.pw>
2023-02-05 16:09:45 +1100

    chore: cleaned up app example

--- examples/commonjs/app.js
+++ examples/commonjs/app.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
 
 const { RouteEngine } = require("../../dist/common/fs-routes.cjs");
+
+function join(...name) {
+    return path.join(__dirname, ...name);
+}
 
 const app = express();
 const port = 5050;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-function join(...name) {
-    return path.join(__dirname, ...name);
-}
 
 const fsRoutes = new RouteEngine(app, "commonjs");
 
@@ -30,11 +29,3 @@ async function startApp() {
 }
 
 startApp();
-
-// (async () => {
-//     fsRoutes.registerRoutes();
-
-//     app.listen(port, () => {
-//         console.log(`Example app listening at http://localhost:${port}`);
-//     });
-// })();
