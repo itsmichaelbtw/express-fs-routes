ac2ffc11b797845ec513a91ce11f4bfca76fd3d9
Michael <me@miikey.pw>
2023-02-05 16:09:28 +1100

    chore: added missing response

--- examples/commonjs/routes/users/[id].js
+++ examples/commonjs/routes/users/[id].js
@@ -17,19 +17,39 @@ router.get("/", getUsers(), async (req, res) => {
 });
 
 // get user avatar
-router.get("/avatar", async (req, res) => {});
+router.get("/avatar", async (req, res) => {
+    res.send("avatar");
+});
 
 // update user avatar
-router.put("/avatar", async (req, res) => {});
+router.put("/avatar", async (req, res) => {
+    res.send("avatar updated");
+});
 
 // get user permissions
-router.get("/permissions", async () => {});
+router.get("/permissions", async (req, res) => {
+    res.send("permissions");
+});
 
 // update user permissions
-router.put("/permissions", async () => {});
+router.put("/permissions", async (req, res) => {
+    res.send("permissions updated");
+});
 
 // delete user
-router.delete("/", async (req, res) => {});
+router.delete("/", async (req, res) => {
+    const id = req.params.id;
+
+    const users = req.users.filter((user) => {
+        return user.id !== id;
+    });
+
+    await fs.promises.writeFile(
+        (path.join(process.cwd(), "..", "database/users.json"), JSON.stringify(users, null, 4))
+    );
+
+    res.send("User deleted!");
+});
 
 module.exports = router;
 module.exports.routeOptions = {
