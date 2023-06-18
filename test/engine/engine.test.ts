4df3c6e8ff158eb54d12c261d9d9031bcf34412a
Michael <itsmichaelbtw@gmail.com>
2023-06-18 20:05:55 +1000

    test: updated unit tests for engine

--- test/engine/engine.test.ts
+++ test/engine/engine.test.ts
@@ -1,10 +1,11 @@
-import type { RouterRegistry, RouterSchema, TreeNode } from "../../lib/types";
+import type { RouteSchema, TreeNode } from "../../lib/types";
 
 import chai from "chai";
 import path from "path";
 import fs from "fs";
 
 import { RouteEngine } from "../../lib";
+import { DEFAULT_OPTIONS } from "../../lib/constants";
 
 import { newRouteEngine } from "../../scripts/route-engine";
 import { appCache, supertest } from "../../scripts/supertest";
@@ -12,7 +13,7 @@ import { destroyAllRoutes } from "../../scripts/route-removal";
 
 const routeEngine = newRouteEngine();
 
-async function expectStatus(route: RouterSchema, status: number): Promise<void> {
+async function expectStatus(route: RouteSchema, status: number): Promise<void> {
   if (route.layers.length > 1) {
     for (const layer of route.layers) {
       await supertest().get(layer.complete_path).expect(status);
@@ -48,9 +49,35 @@ describe("route-engine", () => {
     }
   });
 
+  it("should throw an error if the 'context' is invalid", async () => {
+    try {
+      new RouteEngine(appCache.app, "my random context" as any);
+
+      chai.expect.fail("Should have thrown an error");
+    } catch (error) {
+      chai.expect(error).to.be.an("error");
+    }
+  });
+
+  it("should properly set the `app` and `context`", async () => {
+    // @ts-ignore
+    chai.expect(routeEngine.engine.$app).to.equal(appCache.app);
+    // @ts-ignore
+    chai.expect(routeEngine.engine.$context).to.equal("commonjs");
+  });
+
+  it("should set the default options", async () => {
+    const engine = new RouteEngine(appCache.app, "commonjs");
+
+    // @ts-ignore
+    chai.expect(engine.$options).to.be.an("object");
+    // @ts-ignore
+    chai.expect(engine.$options).to.deep.equal(DEFAULT_OPTIONS);
+  });
+
   describe("when registering routes", () => {
     it("routes that are declared as 'registered' should return a 200", async () => {
-      const registry = await routeEngine.engine.registerRoutes();
+      const registry = await routeEngine.engine.run();
 
       for (const route of registry) {
         if (route.status === "registered" && !route.route_options.paramsRegex) {
@@ -60,7 +87,7 @@ describe("route-engine", () => {
     });
 
     it("routes that are not declared as 'registered' should return a 404", async () => {
-      const registry = await routeEngine.engine.registerRoutes();
+      const registry = await routeEngine.engine.run();
 
       for (const route of registry) {
         if (route.status !== "registered" && !route.route_options.paramsRegex) {
@@ -69,30 +96,30 @@ describe("route-engine", () => {
       }
     });
 
-    it("should throw an error if the file is empty", async () => {
-      try {
-        routeEngine.engine.setOptions(
-          routeEngine.mergeOptions({
-            silent: false
-          })
-        );
-
-        await routeEngine.engine.registerRoutes();
-
-        chai.expect.fail("Should have thrown an error");
-      } catch (error) {
-        chai.expect(error).to.be.an("error");
-      }
-    });
-
     it("should set the status to 'skipped' if the file is empty", async () => {
-      const registry = await routeEngine.engine.registerRoutes();
+      const registry = await routeEngine.engine.run();
 
       const errorRoute = registry.find((route) => route.base_path === null);
 
       chai.expect(errorRoute).to.be.an("object");
       chai.expect(errorRoute?.status).to.equal("skipped");
       chai.expect(errorRoute?.error).to.equal("Most likely forgot to export a default function.");
+    });
+
+    it("should throw an error if the file is empty when using `strictMode`", async () => {
+      try {
+        routeEngine.engine.setOptions(
+          routeEngine.mergeOptions({
+            strictMode: true
+          })
+        );
+
+        await routeEngine.engine.run();
+
+        chai.expect.fail("Should have thrown an error");
+      } catch (error) {
+        chai.expect(error).to.be.an("error");
+      }
     });
   });
 });
